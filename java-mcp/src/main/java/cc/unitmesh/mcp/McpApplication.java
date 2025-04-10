50a6fda407fa1011d94f5a0e4e82a74f25d90cc6
Phodal Huang <h@phodal.com>
2025-04-10 17:04:14 +0800

    chore: init spring from https://start.spring.io/

---
+++ java-mcp/src/main/java/cc/unitmesh/mcp/McpApplication.java
@@ -0,0 +1,13 @@
+package cc.unitmesh.mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpApplication.class, args);
+	}
+
+}
---
+++ java-mcp/src/test/java/cc/unitmesh/mcp/McpApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.unitmesh.mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
